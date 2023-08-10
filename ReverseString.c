6e096cbbc0e3e7d4bc06cfeee59e1e1abda610b1
lingacharyk <113505257+lingacharyk@users.noreply.github.com>
2023-08-10 08:43:31 +0530

    Update ReverseString.c

--- ReverseString.c
+++ ReverseString.c
@@ -2,20 +2,17 @@
 #include <string.h>
 
 int main() {
-  // Get the string from the user
+
   char str[100];
   printf("Enter a string: ");
   gets(str);
 
-  // Reverse the string
   int i, j;
   for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
     char temp = str[i];
     str[i] = str[j];
     str[j] = temp;
   }
-
-  // Print the reversed string
   printf("The reversed string is: %s\n", str);
 
   return 0;
